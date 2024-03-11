8d3ed086661b120876bbb8cd49473e355d321a92
Göktuğ Kola <goktugkola@gmail.com>
2024-03-11 16:39:50 +0300

    bugfix

--- Assets/Ece/Scripts/FollowPlayer.cs
+++ Assets/Ece/Scripts/FollowPlayer.cs
@@ -6,16 +6,22 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player ;
     private Vector3 offset = new Vector3(0,3,-2);
+    private Vector3 playerpos = new Vector3(0, 0, 0);//ehm bunu ben ekledim :p -goktug
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerpos.y = player.transform.position.y;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 newPosition = new Vector3(transform.position.x, player.transform.position.y + offset.y, transform.position.z);
+        if (playerpos.y < (player.transform.position.y + 1f))
+        {
+            playerpos.y = player.transform.position.y;
+        }
+        Vector3 newPosition = new Vector3(transform.position.x, playerpos.y + offset.y, transform.position.z);
         transform.position = newPosition;
         
     }
--- Assets/Goktug/platformSpawner.cs
+++ Assets/Goktug/platformSpawner.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class platformSpawner : MonoBehaviour
 {
+    public GameObject Platforms;
     public GameObject platform;
     public BoxCollider2D spawnArea;
+    [SerializeField] Vector3 spawnerpos = new Vector3(0, 0, 0);
     public BoxCollider2D destroyArea;
     public Camera cam;
     public int platformNumber;
@@ -14,14 +16,17 @@ public class platformSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-
+        transform.position = cam.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = cam.transform.position;
+        if (spawnerpos.y < cam.transform.position.y)
+        {
+            spawnerpos.y = cam.transform.position.y + 2f;
+            transform.position = cam.transform.position;
+        }
         Vector2 min = spawnArea.bounds.min;
         Vector2 max = spawnArea.bounds.max;
         if (platformNumber < minNumber)
@@ -31,8 +36,8 @@ public class platformSpawner : MonoBehaviour
 
             // Spawn the platform at the random position
             GameObject newPlatform = Instantiate(platform, randomPosition, Quaternion.identity);
+            newPlatform.transform.parent = Platforms.transform;
             platformNumber++;
-            print("spawned");
         }
         
 
@@ -40,11 +45,10 @@ public class platformSpawner : MonoBehaviour
     }
      void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.name);
         if (other.CompareTag("Zemin"))
         {
-            print("destroyed");
-            Destroy(platform);
+            Destroy(other.gameObject);
+            print(other.name);
             platformNumber--;
         }
 
