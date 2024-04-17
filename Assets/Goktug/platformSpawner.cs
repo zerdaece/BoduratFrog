6d86464b95fdb83a7501042e202a895161f19b31
Göktuğ Kola <goktugkola@gmail.com>
2024-04-17 17:58:25 +0300

    spawner temizlik

--- Assets/Goktug/platformSpawner.cs
+++ Assets/Goktug/platformSpawner.cs
@@ -8,51 +8,37 @@ public class platformSpawner : MonoBehaviour
     public GameObject Platforms;
     public GameObject platform;
     public BoxCollider2D spawnArea;
-    [SerializeField] Vector3 spawnerpos = new Vector3(0, 0, 0);
-    public BoxCollider2D destroyArea;
-    public Camera cam;
     public int platformNumber;
     public int minNumber;
     public float speed;
-    private Vector2 lastPlatformPosition;
-    
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = cam.transform.position;
+        transform.position = Camera.main.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        transform.position = Vector3.MoveTowards(transform.position, cam.transform.position, speed);
+        transform.position = Vector3.MoveTowards(transform.position, Camera.main.transform.position, speed);
         Vector2 min = spawnArea.bounds.min;
         Vector2 max = spawnArea.bounds.max;
         if (platformNumber < minNumber)
         {
             Vector2 randomPosition;
-
             // Generate random position within the spawn area
-            
-            
-                randomPosition = new Vector2(Random.Range(min.x, max.x), spawnArea.transform.position.y);
-            
-            
-
-
+            randomPosition = new Vector2(Random.Range(min.x, max.x), spawnArea.transform.position.y);
             // Spawn the platform at the random position
             GameObject newPlatform = Instantiate(platform, randomPosition, Quaternion.identity);
             newPlatform.transform.parent = Platforms.transform;
             platformNumber++;
-            
-            // update the last platform position
-            lastPlatformPosition = randomPosition;
         }
-        
+
 
 
     }
