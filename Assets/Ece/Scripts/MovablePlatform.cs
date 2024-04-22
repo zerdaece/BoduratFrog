4a68bc3059e4da96ee667458b9ef347c63bed1f6
Zerda Ece UYĞUN <zerdaeceuygun@posta.mu.edu.tr>
2024-04-22 20:35:05 +0300

    hareketli platform scripti
    sadece platform
    spawnı yok dha

---
+++ Assets/Ece/Scripts/MovablePlatform.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovablePlatform : MonoBehaviour
+{
+    public Transform posA, posB;
+    public float speed;
+    Vector2 targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPos = posB.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, posA.position) < 1f)
+          targetPos= posB.position;
+        if(Vector2.Distance(transform.position, posB.position) < 1f)  
+          targetPos= posA.position;
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);  
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+    }
+}
