20f5437a82b0bff22b1ddd6e3a3b178293516065
Zerda Ece UYĞUN <zerdaeceuygun@posta.mu.edu.tr>
2024-04-25 00:11:58 +0300

    Sound kodu yazıldı
    Müzik kodu içinde yorum satırları açılacak sadece

---
+++ Assets/Ece/Scripts/Sound.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sound : MonoBehaviour
+{
+    private Sprite soundOn;
+    public Sprite soundOff;
+    public Button button;
+    private bool isOn = true;
+    //public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundOn = button.image.sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SoundOnOff()
+    {
+        if (isOn)
+        {
+            button.image.sprite = soundOff;
+            isOn = false;
+            //audioSource.mute = true;
+        }
+        else
+        {
+            button.image.sprite = soundOn;
+            isOn = true;
+            //audioSource.mute = false;
+        }
+    }
+}
